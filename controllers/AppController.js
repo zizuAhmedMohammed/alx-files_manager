7ca645f54958aa81a71e9e17d94e9739457cebc1
Eyerusalem <jerry.gigar@gmail.com>
2023-05-31 07:42:38 -0700

    AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,7 +2,7 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
+class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
@@ -13,7 +13,12 @@ export default class AppController {
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
+        res.status(200).json({
+          users: usersCount,
+          files: filesCount,
+        });
       });
   }
 }
+
+export default AppController;
